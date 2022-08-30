78d03b239d22083a1be59a999fcb6b2122f84e82
unknown <venkatsaiemani24@gmail.com>
2022-08-30 11:51:54 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = 'ADMIN'
+  password = 'ADMIN'
+
+  errorMessage = 'Invalid Credentials'
+  invalidLogin = false
+
+  constructor(private router:Router,
+    private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
+
+    ngOnInit(): void {
+    }
+
+
+
+  handleLogin(){
+    // console.log(this.username);
+    // if(this.username==="ADMIN" && this.password ==="ADMIN")
+    if(this.hardcodedAuthenticationService.authenticate(this.username, this.password)){
+      this.router.navigate(['welcome',this.username])
+      this.invalidLogin = false
+    } else {
+    this.invalidLogin = true
+    }
+
+}
+}
